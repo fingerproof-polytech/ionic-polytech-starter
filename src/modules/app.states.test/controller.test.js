15dc041cfb7f449230d36952439d1b9a3ace47b1
sebastien-p <sp@fing.pro>
2016-11-25 17:06:13 +0100

    Example commit

---
+++ src/modules/app.states.test/controller.test.js
@@ -0,0 +1,17 @@
+/**
+ * @memberOf app.states.test
+ */
+(function (module) {
+  'use strict';
+
+  function TestController($scope) {
+    var controller = this;
+
+
+  }
+
+  module.controller('testController', [
+    TestController
+  ]);
+
+}(angular.module('app.states.test')));
