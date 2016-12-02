959faee4935bed29bebc4db2904cc4cf0580dd3b
sebastien-p <sp@fing.pro>
2016-12-02 21:44:33 +0100

    Display mocked data in search and details screens

--- src/modules/app.states.home/controller.home.js
+++ src/modules/app.states.home/controller.home.js
@@ -4,27 +4,31 @@
 (function (module) {
   'use strict';
 
-  function HomeController($scope, popupService, statesData) {
+  function HomeController($scope, statesService) {
     var controller = this;
 
-    $scope.statesData = statesData;
+    /**
+     * Search model object storing query and results.
+     * @type {Object}
+     */
+    $scope.search = { query: '', results: [] };
 
-    $scope.from = 'homeController';
-
-    controller.showPopup = function () {
-      var scope = { from: $scope.from, title: 'Popup' };
-      popupService.open(module, 'smartphone/popup', scope);
-    };
-
-    controller.onLanguageChange = function (locale) {
-      console.debug(locale);
+    /**
+     * Search for movies matching the `$scope.search.query` value.
+     */
+    controller.search = function () {
+      // Use `statesService.search` which makes an asynchronous
+      // call to the TMDB API and returns a promise...
+      statesService.search($scope.search.query).then(function (results) {
+        // Then, if the call was successful, update `$scope.search.results`.
+        $scope.search.results = results;
+      });
     };
   }
 
   module.controller('homeController', [
     '$scope',
-    'popupService',
-    'statesData',
+    'statesService',
     HomeController
   ]);
 
